e675e4b822cd89c5fc95ab615553c20850520fd8
Blessing Joshua <blessingjoshuaoffl@gmail.com>
2025-01-27 12:01:52 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const app = express()
+const app = express();
 const port = 3000;
 
-app.get('/ping', (req,res) =>{
-    res.send('Pong!');
-})
+app.get('/ping', (req, res) => {
+    try {
+        res.send('Pong!');
+    } catch (error) {
+        res.status(500).send('An error occurred!');
+    }
+});
 
-app.listen(port,()=>{
+app.listen(port, (err) => {
+    if (err) {
+        console.error('Failed to start server:', err);
+        process.exit(1);
+    }
     console.log(`Server is running at http://localhost:${port}`);
 });
