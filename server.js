3d6f09b8759c780bc827af43b54e06c4c05b51b2
Blessing Joshua <blessingjoshuaoffl@gmail.com>
2025-02-12 11:04:36 +0530

    Mongodb client ASAP

--- server.js
+++ server.js
@@ -1,30 +1,15 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
-const connectDatabase = require('./database');
-const port = process.env.PORT;
-
-
-// Connect to database
-connectDatabase();
-
-// Route with enhanced error handling
-app.get('/ping', (req, res) => {
-    try {
-        res.send('Pong!');
-    } catch (error) {
-        console.error('Error occurred in /ping route:', error); // Log error details
-        res.status(500).send('An internal server error occurred!');
-
-    }
-});
-
-// Server startup with improved error handling
-app.listen(port, (err) => {
-    if (err) {
-        console.error(`Error starting server on port ${port}:`, err); // Detailed error message
-        process.exit(1);
-    }
-    console.log(`Server is running at http://localhost:${port}`);
-    
+const express=require('express');
+const {connectDB,getConnection}=require('./database');
+const app= express();
+const port =3000;
+app.get('/ping',(req,res)=>{
+    res.send('Pong!');
+})
+connectDB();
+app.get('/',(req,res)=>{
+    console.log("Connected");
+    res.send(getConnection());
+})
+app.listen(port,()=>{
+    console.log(`server is running at http://localhost:${port}`);
 });
